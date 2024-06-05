328a877b9656003ace399e8d49802ab265659f0d
MeepyMeepMeep <mitsushii12@gmail.com>
2024-06-05 13:33:36 +0200

    Screenshot y query de author.php

--- author.php
+++ author.php
@@ -2,34 +2,89 @@
 
 <?php 
 
-$author = get_queried_object();
-if($author):
+    $author = get_queried_object();
+    if($author):
 ?>
 
-
 <div class="at--profile-header">
-    <div class="at--centered_container">
         <div class="at--profile-header__content">
+            <div class="at--profile-header__avatar">
             <?php echo get_avatar($author->ID, 250);?>
-        
+            </div>
             <div class="at--profile-header__info">
-                <h2><?php echo get_the_author_firstname(); ?> <?php echo get_the_author_lastname(); ?></h2>
-                <p class="mplusr1c">@<?php echo get_the_author_nickname(); ?></p>
-                <a class="mplusr1c" href="<?php echo get_the_author_url(); ?>"></a>
-                <p class="mplusr1c"><?php echo get_the_author_description()?> </p>
+                <h2><?php echo get_the_author_firstname(); ?> <?php echo get_the_author_lastname(); ?>
+                <?php if (function_exists('get_favorites_button')):
+                    echo get_favorites_button();
+                endif;?></h2>
+                <p class="mplusr1c">@<?php echo get_the_author_nickname(); ?> <br>
+                <a href="<?php the_author_url();?>"><?php echo get_the_author_url(); ?></a></p>
+                <p class="mplusr1c-light"><?php echo get_the_author_description()?> </p>
             </div>
         </div>
-    </div>
 </div>
 
-<div class="at--centered_container">
-    <?php if (have_posts()):
-        while (have_posts()): the_post();
-    endwhile;
+<div class="at--grid">
+    <?php
+    if (have_posts()) :
+
+        $paged = get_query_var('paged');
+        $author_id = $author-> ID;
+
+        $posts = array(
+            'author' => $author_id,
+            'post_status' => 'publish',
+            'posts_per_page' => 12,
+            'orderby' => 'date',
+            'order' => 'DESC',
+            'paged' => $paged,
+        );
+
+        $author_posts = new WP_Query( $posts );
+
+        if ( $author_posts->have_posts() ) :
+            while ( $author_posts->have_posts() ) : $author_posts->the_post();
+
+                if ( has_post_thumbnail() ) :?>
+                    <div class="at--miniatura">
+                        <a href="<?php the_permalink(); ?>">
+                            <?php the_post_thumbnail(get_the_ID(), 'large');?>
+                            <div class="jua at--author__overlay">
+                                <p> <?php the_title() ?> </p>
+                            </div>               
+                        </a>
+                    </div>
+                <?php endif;?>
+                    
+            <?php
+            endwhile;?>
+</div>
+
+        <div class="at--centered_container">
+            <div class="at--pagination">
+                <?php if(get_previous_posts_link()):?>
+                    <a href="<?php echo get_previous_posts_page_link();?>">
+                        <button class="jua at--pagination__button">Previous</button>
+                    </a>
+                <?php endif;?>
+                <?php if(get_next_posts_link()):?>
+                    <a href="<?php echo get_next_posts_page_link();?>">
+                        <button class="jua at--pagination__button">Next</button>
+                    </a>
+                <?php endif;?>
+            </div>
+        
+
+        <?php
+        else :
+            echo '<p>No posts by this author.</p>';
+        endif;
+
+        wp_reset_postdata(); 
+    else :
+        echo '<p>No posts found.</p>';
     endif;
     ?>
 </div>
-
-
 <?php endif;
+
 get_footer();?>
